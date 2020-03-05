57ea4a0f86795ad9bfddf3a1380dcd1c150178c6
oscar <provinc@daum.net>
2020-03-05 10:13:24 +0900

    second commit

---
+++ src/components/ToyField.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="toy-field">
+    <h1>toy-field Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'toy-field',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .toy-field {
+
+  }
+</style>
